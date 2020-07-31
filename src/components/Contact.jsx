0315ccab040ae57182c84ab1aeefabb369dd6dcd
NikhilBhardwaj <sharmanikhil65613@gmail.com>
2020-07-31 20:11:40 +0530

    final commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -66,7 +66,6 @@ const ContactForm = () => {
       data-netlify="true"
       data-netlify-honeypot="bot-field"
     >
-      <input type="hidden" name="form-name" value="contact" />
       <Form.Group name="group" controlId="formBasicEmail">
         <Form.Control
           type="text"
